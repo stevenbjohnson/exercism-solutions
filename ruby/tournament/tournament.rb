e7ec1e016f52636e379a3452a7f9200eda0b9a77
joe-sharp <joe.sharp@procore.com>
2021-09-11 17:09:57 -0500

    Tournament Solution 2

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,71 +1,74 @@
 class Tournament
   def self.tally(input)
-    return header if blank?(input)
-
-    results_hash = tally_score(input)
-    create_table(results_hash)
+    new.tally_score(input).create_table
   end
 
-  class << self
-    private
+  def initialize
+    @tournament = {
+      'Allegoric Alaskans'      => { matches_played: 0, wins: 0, draws: 0, losses: 0, points: 0 },
+      'Blithering Badgers'      => { matches_played: 0, wins: 0, draws: 0, losses: 0, points: 0 },
+      'Courageous Californians' => { matches_played: 0, wins: 0, draws: 0, losses: 0, points: 0 },
+      'Devastating Donkeys'     => { matches_played: 0, wins: 0, draws: 0, losses: 0, points: 0 }
+    }
+  end
 
-    def header
-      structure(%w[Team MP W D L P])
-    end
-
-    def blank?(input)
-      !input || input =~ /\A[[:space:]]*\z/
-    end
-
-    def tally_score(input)
-      output = new_tournament
-      input.each_line do |line|
-        home, visitor, result = line.strip.split(';')
-        case result
-        when 'win'
-          output[home][:w] += 1
-          output[home][:p] += 3
-          output[visitor][:l] += 1
-        when 'loss'
-          output[home][:l] += 1
-          output[visitor][:w] += 1
-          output[visitor][:p] += 3
-        when 'draw'
-          output[home][:d] += 1
-          output[home][:p] += 1
-          output[visitor][:d] += 1
-          output[visitor][:p] += 1
-        end
-
-        output[home][:mp] += 1
-        output[visitor][:mp] += 1
+  def tally_score(input)
+    input.each_line do |line|
+      home, visitor, result = line.strip.split(';')
+      case result
+      when 'win'
+        tally_win(winner: home, loser: visitor)
+      when 'loss'
+        tally_win(winner: visitor, loser: home)
+      when 'draw'
+        tally_tie(home, visitor)
       end
-      output
     end
+    self
+  end
 
-    def create_table(results)
-      results.sort_by do |team|
-        [-team.last[:p], team.first]
-      end.reject { |result| result.last[:mp].zero? }.map do |result|
-        structure([result.first, result.last.values].flatten)
-      end.prepend(header).join('')
-    end
+  def create_table
+    header +
+      tournament
+        .sort_by { |result| [-result.last[:points], result.first] }
+        .reject { |result| result.last[:matches_played].zero? }
+        &.map { |result| structure([result.first, result.last.values].flatten) }
+        .join
+  end
 
-    def structure(values)
-      [
-        values.first.ljust(30),
-        values.slice(1,4).map{ |c| c.to_s.rjust(2) },
-        values.last.to_s.rjust(2) + "\n"
-      ].flatten.join(' | ')
-    end
+  private
 
-    def new_tournament
-      {
-        'Allegoric Alaskans'      => { mp: 0, w: 0, d: 0, l: 0, p: 0 },
-        'Blithering Badgers'      => { mp: 0, w: 0, d: 0, l: 0, p: 0 },
-        'Courageous Californians' => { mp: 0, w: 0, d: 0, l: 0, p: 0 },
-        'Devastating Donkeys'     => { mp: 0, w: 0, d: 0, l: 0, p: 0 }
-      }
-    end
+  attr_reader :tournament
+
+  def header
+    structure(%w[Team MP W D L P])
+  end
+
+  def structure(values)
+    [
+      values.first.ljust(30),
+      values.slice(1,4).map { |c| c.to_s.rjust(2) },
+      values.last.to_s.rjust(2) + "\n"
+    ].flatten.join(' | ')
+  end
+
+  def tally_win(winner:, loser:)
+    tournament[winner][:points] += 3
+    tournament[winner][:wins] += 1
+    tournament[loser][:losses] += 1
+    tally_match(winner, loser)
+  end
+
+  def tally_tie(home, visitor)
+    tournament[home][:draws] += 1
+    tournament[home][:points] += 1
+    tournament[visitor][:draws] += 1
+    tournament[visitor][:points] += 1
+    tally_match(home, visitor)
+  end
+
+  def tally_match(home, visitor)
+    tournament[home][:matches_played] += 1
+    tournament[visitor][:matches_played] += 1
   end
 end
