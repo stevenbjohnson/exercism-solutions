a972e617b460fa76c56bff56256abb2b8cf8bf02
Steven Johnson <steven.johnson@procore.com>
2022-11-03 13:37:40 -0700

    Exercism: amusement_park solution

--- ruby/amusement-park/attendee.rb
+++ ruby/amusement-park/attendee.rb
@@ -1,19 +1,12 @@
 class Attendee
+  attr_reader :height, :pass_id
+
   def initialize(height)
     @height = height
-    @pass_id = nil
   end
 
-  def height
-    @height
-  end
-
-  def pass_id
-    @pass_id
-  end
-
-  def issue_pass!(pass_id)
-    @pass_id = pass_id
+  def issue_pass!(id)
+    @pass_id = id
   end
 
   def revoke_pass!
